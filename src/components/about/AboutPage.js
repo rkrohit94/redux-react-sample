236f2957ddcfef87a5168658bec1347f54c51516
rkrohit94 <mailtorkrohit@gmail.com>
2017-05-14 10:10:13 +0530

    initial app

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class AboutPage extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>About</h1>
+        <p>This application is created by Rohit, with help of PluralSight in react and redux</p>
+      </div>
+    )
+  }
+}
+export default AboutPage;
---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CoursesPage extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>CoursesPage</h1>
+      </div>
+    )
+  }
+}
+export default CoursesPage
